53094d5a62158d13e0228f02b77e2eb297724f3a
riothung <mario.thung@gmail.com>
2026-01-28 10:08:07 +0800

    feat: add sidebar, navbar and dashboard to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import "./index.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/layout/Sidebar";
+import Header from "./components/layout/Header";
+import Dashboard from "./pages/Dashboard";
+// import { useState } from "react";
 function App() {
+  // const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  // const toggleSidebar = () => {
+  //   setIsSidebarOpen((prev) => !prev);
+  // };
+
   return (
     <>
-      <h2 className="text-3xl font-bold text-blue-500">Tailwindcss is active!</h2>
+      <Router>
+        <div className="flex">
+          <Sidebar />
+          {/* <header className="p-4 shadow">
+            <button onClick={toggleSidebar} className="rounded-md bg-black px-4 py-2 text-white">
+              Toggle Sidebar
+            </button>
+          </header> */}
+          <div className="flex-1 ml-64">
+            <Header />
+            <main className="p-6">
+              <Dashboard></Dashboard>
+            </main>
+          </div>
+        </div>
+      </Router>
     </>
   );
 }
