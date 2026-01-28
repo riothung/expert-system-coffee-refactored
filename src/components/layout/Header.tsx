52db9331de9856e7ab29d346c0276496b92eba16
riothung <mario.thung@gmail.com>
2026-01-28 10:37:35 +0800

    feat(ui): add navbar

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="bg-white p-4 shadow-md flex justify-end">
+      <div className="flex items-center space-x-2">
+        <span>Feel</span>
+        <div className="w-8 h-8 bg-gray-300 rounded-full"></div> {/* Placeholder untuk ikon user */}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
